34f3a4ab45a89a342a5e5ac5f4a2cd8b26c2f885
Hampus Voong <hampus.voong@learnet.se>
2024-10-10 12:59:20 +0200

    version1

---
+++ Start.py
@@ -0,0 +1,10 @@
+# Skriv en text som berättar var spelaren är och vad den ska göra (print)
+
+# Gör så att spelaren kan attackera motståndaren
+
+# Gör så att motståndaren kan attackera såelaren
+
+# Berätta vem som vann striden
+
+print("Gojo who is an sorcerer is gonna fight against a powerful opponent")
+print("Throughout Heaven and Earth, he is alone the honored one")
